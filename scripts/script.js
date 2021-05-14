bbb30f47cebe291b22e8d10d12bfa51b7bdddaae
Anastasia Raeva <anastassia.raeva@gmail.com>
2021-05-14 22:26:42 +0300

    Add canvas for photo, add button for downloading image

--- scripts/script.js
+++ scripts/script.js
@@ -1,84 +1,109 @@
 "use strict";
 
-//Загрузка файла
-let newPhoto  = document.querySelector('#newPhoto');
-let dropbox = document.querySelector('.photo__dropbox');
-let photoToEdit = document.querySelector('.photo__img');
-
-function changePhotoURl(file) {
-    let fileURL = window.URL.createObjectURL(file);
-    photoToEdit.src = fileURL;
-    file.onload = function() {
-        window.URL.revokeObjectURL(this.src);
-    };
-}
-
-function handleFile() {
-    const file = this.files[0];
-    changePhotoURl(file);
-    resetAllFilters();
-}
-
-function stopDefaultEvent(event) {
-    event.stopPropagation();
-    event.preventDefault();
-}
-
-function drop(event) {
-    stopDefaultEvent(event);
-
-    let file = event.dataTransfer.files[0];
-    changePhotoURl(file);
-    resetAllFilters();
-}
-
-newPhoto.addEventListener('change', handleFile);
-dropbox.addEventListener('dragenter', stopDefaultEvent);
-dropbox.addEventListener('dragover', stopDefaultEvent);
-dropbox.addEventListener('drop', drop);
-
-//Управление слайдерами 
-let ranges = document.querySelectorAll('.tune__range');
-let tunes = new Map();
-
-function handleRangeChange (event) {
-    let range = event.target;
-    let rangeName = range.name;
-    let rangeValue = range.value;
-    let filters = "";
+window.onload = function () {
+    //Создание холста с изображением по умолчанию
+    let photoToEdit = document.querySelector('.photo__img');
+    let canvas = document.querySelector('#canvas');
+    let ctx = canvas.getContext('2d');
+    let width;
+    let height;
     
-    switch(rangeName) {
-        case 'blur':
-            rangeValue = rangeValue + 'px';
-            break;
-        case 'brightness':
-            rangeValue = rangeValue;
-            break;
-        case 'hue-rotate': 
-            rangeValue = rangeValue + 'deg';
-            break;
-        default:
-            rangeValue = rangeValue + '%';
+    width = document.documentElement.clientWidth / 2;
+    height = document.documentElement.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    
+    ctx.drawImage(photoToEdit, 0, 0, width, width * photoToEdit.height / photoToEdit.width);
+
+    //Загрузка файла
+    let newPhoto  = document.querySelector('#newPhoto');
+    let dropbox = document.querySelector('.photo__dropbox');
+
+    function changePhotoURl(file) {
+        let fileURL = window.URL.createObjectURL(file);
+        photoToEdit.src = fileURL;
+        file.onload = function() {
+            window.URL.revokeObjectURL(this.src);
+        };
     }
 
-    if (tunes.has(rangeName)) {
-        tunes.delete(rangeName);
+    function handleFile() {
+        const file = this.files[0];
+        changePhotoURl(file);
+        resetAllFilters();
     }
 
-    tunes.set(rangeName, rangeValue);
-    tunes.forEach((value, key) => filters += ` ${key}(${value})`);
-    photoToEdit.style.filter = filters;
-}
+    function stopDefaultEvent(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
 
-ranges.forEach(item => item.addEventListener('input', handleRangeChange));
+    function drop(event) {
+        stopDefaultEvent(event);
 
-//Сбрасываем фильтры
-let resetButton = document.querySelector('.reset');
+        let file = event.dataTransfer.files[0];
+        changePhotoURl(file);
+        resetAllFilters();
+    }
 
-function resetAllFilters() {
-    photoToEdit.style.filter = '';
-    ranges.forEach((range) => range.value = 0);
-    tunes.clear();
-}
+    newPhoto.addEventListener('change', handleFile);
+    dropbox.addEventListener('dragenter', stopDefaultEvent);
+    dropbox.addEventListener('dragover', stopDefaultEvent);
+    dropbox.addEventListener('drop', drop);
 
-resetButton.addEventListener('click', resetAllFilters);
+    //Управление слайдерами 
+    let ranges = document.querySelectorAll('.tune__range');
+    let tunes = new Map();
+
+    function handleRangeChange (event) {
+        let range = event.target;
+        let rangeName = range.name;
+        let rangeValue = range.value;
+        let filters = "";
+        
+        switch(rangeName) {
+            case 'blur':
+                rangeValue = rangeValue + 'px';
+                break;
+            case 'brightness':
+                rangeValue = rangeValue;
+                break;
+            case 'hue-rotate': 
+                rangeValue = rangeValue + 'deg';
+                break;
+            default:
+                rangeValue = rangeValue + '%';
+        }
+
+        if (tunes.has(rangeName)) {
+            tunes.delete(rangeName);
+        }
+
+        tunes.set(rangeName, rangeValue);
+        tunes.forEach((value, key) => filters += ` ${key}(${value})`);
+        photoToEdit.style.filter = filters;
+    }
+
+    ranges.forEach(item => item.addEventListener('input', handleRangeChange));
+
+    //Сбрасываем фильтры
+    let resetButton = document.querySelector('.reset');
+
+    function resetAllFilters() {
+        photoToEdit.style.filter = '';
+        ranges.forEach((range) => range.value = 0);
+        tunes.clear();
+    }
+
+    resetButton.addEventListener('click', resetAllFilters);
+
+    //Скачивание файла
+    let saveButton = document.querySelector('.save');
+
+    function saveImage() {
+        let url = canvas.toDataURL('image/jpeg');
+        saveButton.href = url;
+    }
+
+    saveButton.addEventListener('click', saveImage);
+};
